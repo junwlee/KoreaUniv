079a36ca4cd9a42f229e7e77c5e8adafee584a32
이준원 <junwlee@ijun-won-ui-MacBookPro.local>
2024-04-26 20:37:44 +0900

    Implement basic bouncing ball game using tkinter
    - Add functionality to move the paddle left and right using keyboard inputs.
    - Implement ball bouncing off the paddle.
    - Ensure the ball bounces back when hitting the walls.
    - End the game when the ball touches the bottom of the window.

---
+++ 24-Spring/python/BouncingBall/BouncingBall.py
@@ -0,0 +1,117 @@
+from tkinter import *
+import random, os, sys
+
+class Ball:
+    def __init__(self, canvas, paddle, color):
+        self.canvas = canvas
+        self.paddle = paddle
+        self.id = canvas.create_oval(10, 10, 25, 25, fill=color) # 공 그리기
+
+        initial_x = self.canvas.winfo_width() * 0.5
+        initial_y = self.canvas.winfo_height() * 0.6
+        self.canvas.move(self.id, initial_x, initial_y) # 공의 위치를 설정하는 정적인 값
+
+        # 공의 시작 방향 변경
+        starts = [i for i in range(-3,4) if i != 0]
+        random.shuffle(starts)
+        self.x = starts[random.randint(0, len(starts) - 1)]
+        self.y = -int(self.canvas.winfo_height() * 0.005)
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+        # self.canvas_height와 self.canvas_width:
+        # 캔버스의 현재 높이와 너비를 저장하여, 공이 캔버스의 경계를 넘어서 이동하지 않도록 하는 데 사용하기 위함
+
+        # 바닥에 닿는 경우 체크
+        self.hit_bottom = False
+
+    def hit_paddle(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+
+        # 공의 오른쪽 면의 위치가 패들의 왼쪽 위치보다 크고,
+        # 공의 왼쪽 위치가 패들의 오른쪽 위치보다 작은지
+        if pos[0] <= paddle_pos[2] and pos[2] >= paddle_pos[0]:
+            if paddle_pos[1] <= pos[3] <= paddle_pos[3]: # 공이 패들보다 공중에 떠있는 경우 제외
+                return True
+        return False
+
+    # 공을 움직이고 경계에 닿으면 방향을 변경
+    def draw(self):
+        self.canvas.move(self.id, self.x, self.y)
+        # self.x와 self.y:
+        # 매 프레임마다 공의 움직임을 제어하는 x축, y축 속도를 지정하기 위해 사용되는 값
+        pos = self.canvas.coords(self.id) # 식별 번호로 캔버스에 그려진 현재의 x와 y좌표를 반환        # pos값은 [x1, y1, x2, y2] 형태의 리스트 값
+        # x1과 y1은 공의 좌측 상단, x2와 y2는 공의 우측 하단
+        if pos[1] <= 0: self.y = 3 # 공을 아래로 방향 전환시키기 위해 self.y의 값을 양수로 조정
+        if pos[3] >= self.canvas_height:
+            self.y = -3
+            self.hit_bottom = True # 공이 바닥에 닿는 경우 패배
+        # 공이 측면에 부딪히는 경우를 고려
+        if pos[0] <= 0: self.x = 3
+        if pos[2] >= self.canvas_width: self.x = -3
+
+        # Paddle에 부딪힌 경우
+        if self.hit_paddle(pos): self.y = -3
+
+class Paddle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+
+        initial_x = self.canvas.winfo_width() * 0.4
+        initial_y = self.canvas.winfo_height() * 0.8
+        self.canvas.move(self.id, initial_x, initial_y)
+
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+
+        # key 바인딩
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0: self.x = 0
+        elif pos[2] >= self.canvas_width: self.x = 0
+
+    def turn_left(self, event): self.x = -int(self.canvas_width * 0.003)
+    def turn_right(self, event): self.x = int(self.canvas_width * 0.003)
+
+def restart(event):
+    global path
+    global tk
+    try:
+        if ('normal' == tk.state()):
+            tk.destroy()
+    finally:
+        os.system(f'python "{path}"')
+
+def game_loop():
+    if not ball.hit_bottom:
+        ball.draw()
+        paddle.draw()
+    tk.after(10, game_loop) # 10ms = 초당 100번 함수 호출
+
+path = os.path.realpath(sys.argv[0])
+
+# Frame 생성
+tk = Tk() # Tk 객체: 버튼, 입력상자, 그림 그릴 수 있는 캔버스 제공
+tk.title("Bouncing Ball")
+tk.resizable(0, 0) # 고정된 프레임 크기, 사이즈 변경 불가
+tk.wm_attributes("-topmost", 1) # 다른 모든 창들 앞에 캔버스를 가진 창이 위치함
+canvas = Canvas(tk, width=800, height=500, bd=0, highlightthickness=0)
+# bd=0, highlightthickness=0 => 캔버스 외곽에 둘러싼 외곽선(border)이 없도록
+canvas.pack() # 위 설정값대로 크기를 맞춤
+canvas.bind_all("<KeyPress-Return>", restart)
+tk.update() # Tkinter가 변경된 GUI 요소들을 즉시 처리하고 화면에 그 결과를 반영
+btn = Button(tk, text="Quit", command=tk.quit) # 'Quit' 버튼에 tk.quit 함수 연결
+btn.place(x=5, y=5, anchor="nw")
+# btn.pack() # 화면에 표시하라는 지시 명령
+# 버튼이 기본적으로 캔버스 내에서 자동적으로 정렬
+
+paddle = Paddle(canvas, 'blue')
+ball = Ball(canvas, paddle, 'red')
+
+tk.after(10, game_loop)
+tk.mainloop() # 이벤트 루프 시작
