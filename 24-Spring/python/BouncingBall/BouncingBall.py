e1371457a403dd43bab1fec51f15cd495b09e0c2
이준원 <junwlee@ijun-won-ui-MacBookPro.local>
2024-05-25 17:25:34 +0900

    Add collision detection for ball hitting the top paddle & change variable name
    - Implemented a check to determine if the ball is moving upwards and collides with the top paddle.
    - If a collision is detected, the ball's direction is reversed and the score is increased.

--- 24-Spring/python/BouncingBall/BouncingBall.py
+++ 24-Spring/python/BouncingBall/BouncingBall.py
@@ -38,7 +38,7 @@ class Ball:
         # 캔버스의 현재 높이와 너비를 저장하여, 공이 캔버스의 경계를 넘어서 이동하지 않도록 하는 데 사용하기 위함
 
         # 바닥에 닿는 경우 체크
-        self.hit_floor = False
+        self.touch_floor = False
 
     def hit_bottom(self, pos):
         paddle_pos = self.canvas.coords(self.paddle.id)
@@ -69,7 +69,7 @@ class Ball:
         if pos[1] <= 0: self.y = 3 # 공을 아래로 방향 전환시키기 위해 self.y의 값을 양수로 조정
         if pos[3] >= self.canvas_height:
             self.y = -3
-            self.hit_floor = True # 공이 바닥에 닿는 경우 패배
+            self.touch_floor = True # 공이 바닥에 닿는 경우 패배
         # 공이 측면에 부딪히는 경우를 고려
         if pos[0] <= 0: self.x = 3
         if pos[2] >= self.canvas_width: self.x = -3
@@ -78,7 +78,7 @@ class Ball:
         if self.hit_bottom(pos): self.y = -3
 
         # TopPaddle에 부딪힌 경우
-        if self.hit_top(pos):
+        if self.y < 0 and self.hit_top(pos):
             self.y = 3
             self.scoreboard.increase_score()
 
@@ -137,7 +137,7 @@ def restart(event):
         os.system(f'python "{path}"')
 
 def game_loop():
-    if not ball.hit_floor:
+    if not ball.touch_floor:
         ball.draw()
         paddle.draw()
         top.draw()
